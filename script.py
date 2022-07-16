7eeae3c17d96177742b985f42d33a2ba704e0eb9
hom237 <87473852+hom237@users.noreply.github.com>
2022-07-16 16:21:16 +0900

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://www.facebook.com/profile.php?id=100042266040723" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -21,19 +21,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["green", "blue"]
+colors = ["white", "blue"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "이진식")
+write("description", "관천중학교")
+write("button", "페이스북")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "생일": "2/10",
+  "좋아하는것": "만화, 건담",
+  "싫어하는것": "(빠른)포기?",
+  "처음시작한 언어": "파이썬"
 }
 information(informations)
